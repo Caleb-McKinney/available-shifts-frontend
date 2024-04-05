118f6f04e71e2950cf3664aef3d9b192e5e8f691
Caleb McKinney <McKinney.Caleb@gmail.com>
2024-04-05 13:23:58 -0500

    updated about page and added styling

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return {
+    <>
+    <header>
+    <nav>
+    <h1>Available Shifts App</h1>
+    </nav>
+    </header>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div>
+    <h2>Available Shifts</h2>
+    </div>
+    </main>
+    </>
+  };
+}
